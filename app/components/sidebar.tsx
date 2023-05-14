9cf24b99836a83a40d557c4c575f71209a5ee637
shenjianxiaoran <351470611@qq.com>
2023-05-14 16:27:37 +0800

    Update sidebar.tsx
    更改名称

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Ctrpts ChatGpt</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Ctrpts欢迎您
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />

8072223b0a01e723fede208cfd4122ed32175465
shenjianxiaoran <351470611@qq.com>
2023-05-15 22:08:08 +0800

    Update layout.tsx
    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ctrpts chatgpt",
+  description: "Ctrpts ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ctrpts chatgpt",
     statusBarStyle: "default",
   },
 };
